32341ad9af08b75836469a13c07c63e62c36ecd9
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-08-19 22:27:54 +0900

    :new: とりあえず実行出来るように

---
+++ Assets/Scripts/Inui.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace HK.Inui
+{
+    public sealed class Inui : MonoBehaviour
+    {
+        [SerializeField]
+        private Text outputText;
+
+        public void Run(string source)
+        {
+            this.outputText.text = source;
+        }
+    }
+}
---
+++ Assets/Scripts/RunButtonController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace HK.Inui
+{
+    public sealed class RunButtonController : MonoBehaviour, IPointerUpHandler
+    {
+        [SerializeField]
+        private Inui inui;
+
+        [SerializeField]
+        private InputField inputField;
+
+        void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+        {
+            this.inui.Run(this.inputField.text);
+        }
+    }
+}
