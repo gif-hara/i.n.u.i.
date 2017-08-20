9e16958ddf6bca9ac217543eaaeea5db0f443221
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-08-20 20:40:10 +0900

    :new: テンプレートボタン追加

---
+++ Assets/Scripts/TemplateButtonController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+namespace HK.Inui
+{
+    public sealed class TemplateButtonController : MonoBehaviour, IPointerUpHandler
+    {
+        [SerializeField][Multiline]
+		private string template;
+		
+        [SerializeField]
+        private InputField inputField;
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            this.inputField.text = this.template;
+        }
+    }
+}
