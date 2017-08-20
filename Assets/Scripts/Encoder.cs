07978f976f652a4b0825b10310c080d4b055aa1a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-08-21 01:46:50 +0900

    :new: エンコーダ実装

--- Assets/Scripts/Encoder.cs
+++ Assets/Scripts/Encoder.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
-
+using System.Text;
+using System.Linq;
 namespace HK.Inui
 {
     /// <summary>
@@ -9,7 +10,19 @@ namespace HK.Inui
     {
         public static string Encode(string message)
         {
-            return message;
+            var result = new StringBuilder();
+            var current = 0;
+            foreach(var c in message)
+            {
+                var diff = c - current;
+                var absDiff = Mathf.Abs(diff);
+                var isPositive = diff > 0;
+                var word = isPositive ? Inui.ReservedWord.Increment : Inui.ReservedWord.Decrement;
+                result.AppendLine(string.Concat(Enumerable.Repeat(word, absDiff).ToArray()));
+                result.AppendLine(Inui.ReservedWord.Echo);
+                current = c;
+            }
+            return result.ToString();
         }
     }
 }
