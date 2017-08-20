7cd5a5373dac07d0c510ad0fb85bd74cb42cdebf
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-08-20 19:48:23 +0900

    :new: 予約語を解析出来るように

--- Assets/Scripts/Inui.cs
+++ Assets/Scripts/Inui.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
 
 namespace HK.Inui
 {
@@ -8,9 +10,58 @@ namespace HK.Inui
         [SerializeField]
         private Text outputText;
 
-        public void Run(string source)
+        private readonly string[] ReservedWords = new string[]
         {
-            this.outputText.text = source;
+            "マ？", // ポインタの指すメモリの値を1増やす
+            "がちやな", // ポインタの指すメモリの値を1減らす
+            "あるなぁ", // ポインタを右に1つずらす
+            "あるか？", // ポインタを左に1つずらす
+            "僕っすかぁ？W", // ポインタの指すメモリの値が0だったら次の「何すかぁ？W」に進む
+            "何すかぁ？W", // ポインタの指すメモリの値が0でなければ前の「僕っすかぁ？W」に戻る
+            "そういうとこよ", // 出力
+        };
+
+        public class SortWord
+        {
+            public int Index;
+
+            public string Word;
         }
+
+        public string Run(string source)
+        {
+            return source;
+        }
+
+        /// <summary>
+        /// 予約語のみのソートされたリストを返す
+        /// </summary>
+        private List<SortWord> GetSortedWords(string source)
+        {
+			var result = new List<SortWord>();
+
+			for (int i = 0; i < ReservedWords.Length; ++i)
+			{
+				var reservedWord = ReservedWords[i];
+				int sourceIndex = 0;
+				while (source.Length > sourceIndex)
+				{
+					var indexOf = source.IndexOf(reservedWord, sourceIndex, StringComparison.CurrentCulture);
+					if (indexOf != -1)
+					{
+						result.Add(new SortWord() { Index = indexOf, Word = reservedWord });
+						sourceIndex = indexOf + reservedWord.Length;
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+
+			result.Sort((x, y) => x.Index - y.Index);
+
+            return result;
+		}
     }
 }
