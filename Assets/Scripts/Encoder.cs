b5adf75900a1ad1e700a7ad9f0e18e89faa1c67e
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-08-21 01:16:02 +0900

    :new: エンコーダ定義

---
+++ Assets/Scripts/Encoder.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace HK.Inui
+{
+    /// <summary>
+    /// Inuinf*ck言語に変換するクラス
+    /// </summary>
+    public static class Encoder
+    {
+        public static string Encode(string message)
+        {
+            return message;
+        }
+    }
+}
