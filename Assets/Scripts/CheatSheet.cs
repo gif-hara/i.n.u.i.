599b596707df62fca068fb589a8b6d4ea7f300d3
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-08-21 07:16:20 +0900

    :bulb: チートシートにショートカットキーを表示する

--- Assets/Scripts/CheatSheet.cs
+++ Assets/Scripts/CheatSheet.cs
@@ -12,14 +12,14 @@ namespace HK.Inui
         void Start()
         {
             var builder = new StringBuilder();
-			builder.AppendLine(string.Format("{0} = ポインタの指すメモリの値を1増やす", Inui.ReservedWord.Increment));
-            builder.AppendLine(string.Format("{0} = ポインタの指すメモリの値を1減らす", Inui.ReservedWord.Decrement));
-			builder.AppendLine(string.Format("{0} = ポインタを右に1つずらす", Inui.ReservedWord.MoveRight));
-			builder.AppendLine(string.Format("{0} = ポインタを左に1つずらす", Inui.ReservedWord.MoveLeft));
-			builder.AppendLine(string.Format("{0} = ポインタの指すメモリの値が0だったら次の\"{1}\"へ移動する", Inui.ReservedWord.WhileStart, Inui.ReservedWord.WhileEnd));
-			builder.AppendLine(string.Format("{0} = 現在の状態から一番手前の\"{1}\"へ戻る", Inui.ReservedWord.WhileEnd, Inui.ReservedWord.WhileStart));
-			builder.AppendLine(string.Format("{0} = ポインタの指すメモリの値を出力する", Inui.ReservedWord.Echo));
-            builder.AppendLine(string.Format("{0} = 現在の状態をデバッグ表示する", Inui.ReservedWord.Print));
+			builder.AppendLine(string.Format("\'+\' {0} = ポインタの指すメモリの値を1増やす", Inui.ReservedWord.Increment));
+            builder.AppendLine(string.Format("\'-\' {0} = ポインタの指すメモリの値を1減らす", Inui.ReservedWord.Decrement));
+			builder.AppendLine(string.Format("\'>\' {0} = ポインタを右に1つずらす", Inui.ReservedWord.MoveRight));
+			builder.AppendLine(string.Format("\'<\' {0} = ポインタを左に1つずらす", Inui.ReservedWord.MoveLeft));
+			builder.AppendLine(string.Format("\'[\' {0} = ポインタの指すメモリの値が0だったら次の\"{1}\"へ移動する", Inui.ReservedWord.WhileStart, Inui.ReservedWord.WhileEnd));
+			builder.AppendLine(string.Format("\']\' {0} = 現在の状態から一番手前の\"{1}\"へ戻る", Inui.ReservedWord.WhileEnd, Inui.ReservedWord.WhileStart));
+			builder.AppendLine(string.Format("\'.\' {0} = ポインタの指すメモリの値を出力する", Inui.ReservedWord.Echo));
+            builder.AppendLine(string.Format("\'D\' {0} = 現在の状態をデバッグ表示する", Inui.ReservedWord.Print));
             this.text.text = builder.ToString();
         }
     }
