7967ee63dc41281da34c509710fe30cf3be4df73
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2017-08-21 07:12:43 +0900

    :new: ToggleShortcutButton追加

---
+++ Assets/Scripts/ToggleShortcutButtonController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace HK.Inui
+{
+    public sealed class ToggleShortcutButtonController : MonoBehaviour, IPointerUpHandler
+    {
+        [SerializeField]
+        private ShortcutModeController controller;
+
+        [SerializeField]
+        private Text buttonText;
+
+		[SerializeField]
+		private string enableText;
+
+		[SerializeField]
+		private string disableText;
+
+        void Start()
+        {
+            this.UpdateButtonText();
+        }
+
+		void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+        {
+            this.controller.enabled = !this.controller.enabled;
+            this.UpdateButtonText();
+        }
+
+        private void UpdateButtonText()
+        {
+			this.buttonText.text = this.controller.enabled ? this.enableText : this.disableText;
+		}
+    }
+}
